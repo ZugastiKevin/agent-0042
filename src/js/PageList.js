35e0a3f8e7ab1eed7098f399521ccfb7b4ec6d5c
ZugastiKevin <rochambeaut@gmail.com>
2021-04-27 15:02:17 +0200

    v1

---
+++ src/js/PageList.js
@@ -0,0 +1,13 @@
+const PageList = (argument = "") => {
+  const render = () => {
+    pageContent.innerHTML = `
+      <section class="page-list">
+        <div class="articles">Hey, this page is a PageList template, about : ${argument}</div>
+      </section>
+    `;
+  };
+  render();
+};
+
+export { PageList };
+
